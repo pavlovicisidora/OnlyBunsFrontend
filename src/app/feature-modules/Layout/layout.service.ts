f8e2c8d6f2a00e6538a590f00ff60af9b4b35988
Srxx42 <srdjan.vukojevic002@gmail.com>
2024-11-07 16:02:54 +0100

    Dodat kostur frontend aplikacije, folderi i HOME i NAVBAR moduli

---
+++ src/app/feature-modules/Layout/layout.service.ts
@@ -0,0 +1,7 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+  })
+  export class LayoutService {}
